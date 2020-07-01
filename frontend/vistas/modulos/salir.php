e27e6b4027fb6f5bd752bf596496778e28c3f822
angel44x <70105696@continental.edu.pe>
2020-07-01 15:54:23 -0500

    ultimo avance

---
+++ frontend/vistas/modulos/salir.php
@@ -0,0 +1,15 @@
+<?php
+
+session_destroy();
+
+$url = Ruta::ctrRuta();
+
+
+
+echo '<script>
+	
+	localStorage.removeItem("usuario");
+	localStorage.clear();
+	window.location = "'.$url.'";
+
+</script>';
